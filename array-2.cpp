bda4a5f959d2a31a10b3c8bf76642acabb3977a6
Mohammed sufiyan <132118766+Sufi065@users.noreply.github.com>
2025-04-03 23:10:11 +0530

    Update array-2.cpp

--- array-2.cpp
+++ array-2.cpp
@@ -44,5 +44,6 @@ int main()
     cout<<"USN is:-"<<s.getstudentusn()<<endl;
     cout<<"Heghit is:-"<<s.getheight()<<"cm"<<endl;
     cout<<"weghit is:-"<<s.getweight()<<"kg"<<endl;
+    cout
     return 0;
 }
