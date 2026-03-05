fdd0d5c1bafd0d5622e34b99122d6d753f361b17
Mohammed sufiyan <132118766+Sufi065@users.noreply.github.com>
2026-03-05 09:54:20 +0530

    Implement spiral order traversal of a matrix

--- 54. Spiral Matrix.cpp
+++ 54. Spiral Matrix.cpp
@@ -1,1 +1,35 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int scol=0, srow=0, erow=m-1, ecol=n-1;
+
+        while(srow <= erow && scol <= ecol){
+
+        for(int i=scol; i<=ecol; i++){
+            ans.push_back(matrix[srow][i]);
+        }
+         for(int i=srow+1; i<=erow; i++){
+            ans.push_back(matrix[i][ecol]);
+        }
+         for(int i=ecol-1; i>=scol; i--){
+            if(srow == erow){
+                break;
+            }
+            ans.push_back(matrix[erow][i]);
+        }
+         for(int i=erow-1; i>=srow+1; i--){
+             if(scol == ecol){
+                break;
+            }
+            ans.push_back(matrix[i][scol]);
+        }
+        srow++; scol++;
+        erow--; ecol--;
+        }
+        return ans;
+    }
+};
 
