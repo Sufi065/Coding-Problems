4754b2d19c9540b3e86380eb7e67a0a2bc5f32dc
Mohammed sufiyan <132118766+Sufi065@users.noreply.github.com>
2026-04-21 17:52:11 +0530

    Add Car class and kClosest function implementation
    Implement kClosest function to find k closest points to origin using a max heap.

---
+++ Car.cpp
@@ -0,0 +1,43 @@
+class Car {
+public:
+    int idx;
+    int dist;
+
+    Car(int idx, int dist){
+        this->idx = idx;
+        this->dist = dist;
+    }
+
+    bool operator<(const Car &obj) const {
+        return this->dist < obj.dist; // max heap
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<Car> pq;
+
+        for(int i = 0; i < points.size(); i++){
+            int x = points[i][0];
+            int y = points[i][1];
+
+            int dist = x*x + y*y;
+            pq.push(Car(i, dist));
+
+            if(pq.size() > k){
+                pq.pop(); // remove farthest
+            }
+        }
+
+        vector<vector<int>> result;
+
+        while(!pq.empty()){
+            int idx = pq.top().idx;
+            pq.pop();
+            result.push_back(points[idx]);
+        }
+
+        return result;
+    }
+};
