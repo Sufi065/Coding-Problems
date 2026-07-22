c28d4ede6cc103aef5b246bc49b56592261b8174
Mohammed sufiyan <132118766+Sufi065@users.noreply.github.com>
2026-07-22 17:22:25 +0530

    Add sum tree transformation logic
    Implement function to convert a binary tree to a sum tree.

--- Transform to Sum Tree.cpp
+++ Transform to Sum Tree.cpp
@@ -1,1 +1,31 @@
+/* Structure for Tree Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
 
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+  
+  int func(Node* root){
+      if(root==NULL)return 0;
+      int lft = func(root->left);
+      int rht = func(root->right);
+      int x = root->data;
+      root->data = lft+rht;
+      return lft+rht+x;
+  }
+    void toSumTree(Node *node) {
+        // code here
+        func(node);
+    }
+};
